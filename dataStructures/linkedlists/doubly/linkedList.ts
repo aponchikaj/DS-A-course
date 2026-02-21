a8f284ef48b3e3e036ff147d0547bab9b2d43cfa
aponchikaj <lazzexa@gmail.com>
2026-02-21 21:02:18 +0400

    made addToStart function

--- dataStructures/linkedlists/doubly/linkedList.ts
+++ dataStructures/linkedlists/doubly/linkedList.ts
@@ -1,17 +1,64 @@
-class ListNode<T> {
+// so in doubly linked lists nodes has previous string too
+class DoublyListNode<T> {
     data:T;
-    next: ListNode<T> | null = null;
-    prev: ListNode<T> | null = null;
+    next: DoublyListNode<T> | null = null;
+    prev: DoublyListNode<T> | null = null;
 
     constructor(data:T){
         this.data = data;
     }
 }
 
-class LinkedList<T> {
-    head: ListNode<T> | null = null
-    tail: ListNode<T> | null = null
+// doubly linked lists have tail - last node and size - sizeof linkedlist 
+class DoublyLinkedList<T> {
+    head: DoublyListNode<T> | null = null
+    tail: DoublyListNode<T> | null = null
     size: number = 0
 
-    
+    // so add to end function and has parameter named data
+    // first we make newNode with doublyListNode
+    // then check if we already have tail
+    // if we dont then head = newNode and tail = newNode
+    // else if we have tail then newNode.prev = tail
+    // tail.next = newNode
+    // and tail = newNode
+    // and then size is increased
+    addToEnd(data:T){
+
+        const newNode = new DoublyListNode(data);
+
+        if(!this.tail){
+            this.head = newNode
+            this.tail = newNode
+        }else{
+            newNode.prev = this.tail
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+
+        this.size++
+    }
+
+    // add to start function.
+    // data is getting as head with this function
+    // we make variable newNode 
+    // check if !head if !head then head and tail = newNode
+    // else if there is valid head
+    // newNode.next = this.head
+    // and head and head.prev = newNode
+    // and size++
+    addToStart(data:T){
+        const newNode = new DoublyListNode(data);
+        
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode;
+        }else{
+            newNode.next = this.head
+            this.head.prev = newNode
+            this.head = newNode
+        }
+
+        this.size++
+    }
 }
--- dataStructures/linkedlists/singly/linkedList.ts
+++ dataStructures/linkedlists/singly/linkedList.ts
@@ -2,9 +2,9 @@
 // structure of that train is like : node(data,next)->node(data,next)... and so on...
 // next is pointing to next node and data has the data that we added to that node.
 
-class ListNode<T> {
+class SinglyListNode<T> {
     data:T
-    next: ListNode<T> | null = null
+    next: SinglyListNode<T> | null = null
 
     constructor(data:T){
         this.data = data
@@ -12,9 +12,9 @@ class ListNode<T> {
 }
 
 //
-class LinkedList<T> {
+class SinglyLinkedList<T> {
     // starting point.
-    head: ListNode<T> | null = null;
+    head: SinglyListNode<T> | null = null;
 
     // this is add function to add new nodes to list
     // we pass data as parameter and its type is T - T is type of LinkedList
@@ -22,7 +22,7 @@ class LinkedList<T> {
     // if we dont have starting point of list (head) then we assign head the newNode so newNode is gonna be starting node
     // but if we have head available we start from head and get next to each node till we get to null and then we assign current.next newNode and thats how we add new node to list.
     add(data:T){
-        const newNode = new ListNode(data);
+        const newNode = new SinglyListNode(data);
         if(!this.head){
             this.head = newNode
         }else{
@@ -100,8 +100,8 @@ class LinkedList<T> {
             return;
         }
 
-        let current: ListNode<T> | null = this.head;
-        let previous: ListNode<T> | null = null;
+        let current: SinglyListNode<T> | null = this.head;
+        let previous: SinglyListNode<T> | null = null;
         let currentIndex = 0;
 
         while(current !== null && currentIndex < index){
@@ -134,7 +134,7 @@ class LinkedList<T> {
             return;
         }
 
-        const newNode = new ListNode(data);
+        const newNode = new SinglyListNode(data);
 
         if(index == 0){
             newNode.next = this.head;
@@ -271,7 +271,7 @@ class LinkedList<T> {
 
 }
 
-const list = new LinkedList<number>()
+const list = new SinglyLinkedList<number>()
 
 list.add(1)
 list.add(2)
