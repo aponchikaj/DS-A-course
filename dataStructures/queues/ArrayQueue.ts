7366168bcc769da133a02aa50f62f665ee3fde92
aponchikaj <lazzexa@gmail.com>
2026-02-27 13:02:54 +0400

    made array queue again from scratch

--- dataStructures/queues/ArrayQueue.ts
+++ dataStructures/queues/ArrayQueue.ts
@@ -1,31 +1,19 @@
-// this is basic array queue class working with array easy but slower.
 class ArrayQueue<T> {
-    private data:T[] = []
-    
+    private data: T[] = [];
+
     enqueue(data:T){
         this.data.push(data)
     }
 
     dequeue(){
-        return this.data.shift()
+        this.data.shift()
     }
 
     peek(){
-        return this.data[0]
+        return this.data[0];
     }
 
-    isEmpty(){
-        return this.data.length === 0 ? true : false
-    }
-
-    search(data:any){
-        for(let i =0;i<this.data.length;i++){
-            if(this.data[i] == data){
-                return true
-            }
-        }
-
-        return false
+    size(){
+        return this.data.length;
     }
 }
-
