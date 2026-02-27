b85a3d77b8e05e1b5a1b4264f1bb74d6f3c9a138
aponchikaj <lazzexa@gmail.com>
2026-02-27 13:30:47 +0400

    made enqueue function in linkedqueue

--- dataStructures/queues/linkedqueue.ts
+++ dataStructures/queues/linkedqueue.ts
@@ -1,54 +1,32 @@
 class QNode<T>{
-    data:T
-    next: QNode<T> | null = null
-    perv: QNode<T> | null = null
+    data:T;
+    next: QNode<T> | null = null;
+    prev: QNode<T> | null = null;
 
     constructor(data:T){
-        this.data = data
+        this.data = data;
     }
 }
 
-class LinkedQueue<T>{
-    private head:QNode<T> | null = null;
-    private tail:QNode<T> | null = null;
+class LinkedQueue<T> {
+    private head: QNode<T> | null = null;
+    private tail: QNode<T> | null = null;
     private length: number = 0;
-    
+
     enqueue(data:T){
-        const newNode = new QNode(data)
-
-        if(this.tail){
-            this.tail.next = newNode
-        }
-
-        this.tail =newNode
+        const newNode = new QNode(data);
 
         if(!this.head){
+
             this.head = newNode
+            this.tail = newNode
+        }else{
+            this.tail!.next = newNode
+            newNode.prev = this.tail
+            this.tail = newNode
         }
 
         this.length++
-    }
-
-    dequeue(){
-        if(!this.head) return null;
-
-        const deletedValue = this.head.data
-        this.head = this.head.next
-
-        if(!this.head){
-            this.tail = null;
-        }
-
-        this.length--
-
-        return deletedValue
-    }
-
-    peek(){
-        return this.head?.data
-    }
-
-    size(){
-        return this.length;
+        return;
     }
 }
