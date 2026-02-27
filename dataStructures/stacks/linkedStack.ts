9c00dec6c7ad86b2131b4323ec9991adb666c7dd
aponchikaj <lazzexa@gmail.com>
2026-02-27 12:14:56 +0400

    made pop function

--- dataStructures/stacks/linkedStack.ts
+++ dataStructures/stacks/linkedStack.ts
@@ -30,5 +30,18 @@ class LinkedStack<T>{
         return;
     }
 
+    // pop function
+    // so we check if we have head if we dont then we return null
+    // then we create poppedData variable and assign this.head.data to it
+    // then our head becomes head.next 
+    // the next node from head
+    // and return poppedData variable
+    pop(){
+        if(!this.head) return null;
 
+        const poppedData = this.head.data
+        this.head = this.head.next;
+
+        return poppedData
+    }
 }
